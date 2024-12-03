45663e89d7dd707281bf942f9f0620e18fc82817
maxnovak-uk <max.novak@reflexmd.com>
2024-12-03 19:01:51 +0200

    Added button types

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,6 +1,17 @@
 import React from 'react'
 
-export default function Button({children, text, isDisabled, addProductToCart}) {
+export default function Button(
+  {
+    children,
+    text,
+    isDisabled, 
+    addProductToCart
+  }: {
+    children: React.ReactNode,
+    text: string,
+    isDisabled: boolean, 
+    addProductToCart: () => {}
+}) {
   return (
     <button disabled={isDisabled} className="inline-flex gap-x-2 items-center rounded-md bg-indigo-500 transition-colors px-4 py-3 uppercase text-sm font-semibold text-white lg:hover:bg-indigo-700" onClick={addProductToCart}>
       {children}
